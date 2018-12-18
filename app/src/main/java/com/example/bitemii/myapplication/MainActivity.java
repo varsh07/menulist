972a816f0a915a0848da2484431ac1b786db943b
varsh07 <varshinisrii19@gmail.com>
2018-12-18 17:43:56 +0530

    first commit

---
+++ app/src/main/java/com/example/bitemii/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.bitemii.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+}
